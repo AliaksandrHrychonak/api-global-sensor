dee9e86848b0dc0c0edf9e49870864606b990337
Aliaksandr Hrychonak <alexsanderkorn90@gmail.com>
2022-01-24 18:37:19 +0300

    f

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const { Router } = require('express');
 const router = new Router();
 const ApiError = require('../exceptions/api-error');
 const errorConfig = require('../utils/error-config');
+const userController = require('../controllers/user-controller');
+const { validateFeedbackForm } = require('../middlewares/validation-handler');
 
 const authRoutes = require('./auth');
 const userRoutes = require('./user');
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
+router.use('/contact', validateFeedbackForm, userController.sendFeedbackForm)
 router.use('*', () => { throw ApiError.NotFoundError(errorConfig.not_found_error); });
 
 module.exports = router;
